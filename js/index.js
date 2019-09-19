247de89a7421a9837f47d1f9f54d06064cf7ba53
Ceejay <emmanuelezeako@yahoo.com>
2019-09-19 11:14:14 +0100

    worked on Ajax CRUD

--- js/index.js
+++ js/index.js
@@ -1,42 +1,46 @@
 
-$(document).ready(function (){
-    $("#postme").click(function(){
-		let input = JSON.stringify({Company:'Etisalat', 'job title':'G Manager',Location:'Ibadan'});
-        $.ajax({
-            type:"POST",
-            dataType:"json",
-            url:"http://localhost:3000/Jobs",
-            data:input,
-            success: function(data){
-                alert("Worked");
-			}
-        });
-    });
-});
-
 $(document).ready(function(){
-    $('#viewall').click(function(){
-        $.ajax({
-            method: 'GET',
-            url:   'http://localhost:3000/employees'
-        }).done(function(data){
-            let JobsAvailable='';
-                    $.each(data, function(i,ignore){
-                        JobsAvailable += '<tr><td>'
-                                    +data[i]["firstname"]+'</td><td>'
-                                    +data[i]["lastname"]+'</td><td>'
-                                    +data[i]["role"]+'</td><td>'
-                                    +data[i]["salary-status"]+'</td><td>'
-                                    +'<button class="btn btn-info">Edit</button>'
-                                    +'<button class="btn btn-info">Pay</button>'
-                                    +'<button class="btn btn-info">Delete</button></td>'
-                                '</tr>';
-
-                    })
-                $('#targ').html(JobsAvailable);
-        })
+    let dynamicURL = '';
+    let methodName = '';
+    displayAll();
+    $("#post").click(function(e){
+        let input = {firstname: $("#a").val(), lastname: $("#b").val(), role: $("#c").val(), 'salary-status': 'Not Paid'};
+        let employeeId = $("#hidden").val();
+        
+        if(employeeId){
+            //update it
+            dynamicURL = 'http://localhost:3000/employees/'+employeeId;
+            methodName = 'PUT';
+        }else{
+            //create as new
+            dynamicURL = 'http://localhost:3000/employees';
+            methodName = 'POST';
+        }
+        if($("#a").val() !== '' && $("#c").val() !== '' && $("#c").val() !== ''){
+            $.ajax({
+                type: methodName,
+                url: dynamicURL,
+                data: input,
+                success: function(value){
+                    displayAll();
+                    methodName === 'POST' ? alert('New employee created successfuly') : alert('Updated successfully');
+                    
+                //    $("#targ").append('<tr><td>'+
+                //    value["firstname"]+'</td><td>'+
+                //    value["lastname"]+'</td><td>'+
+                //    value["role"]+'</td><td>'+
+                //    value["salary-status"]+'</td><td>'+
+                //    '<button class="btn btn-info">Edit</button>'+
+                //    '<button class="btn btn-info">Pay</button>'+
+                //    '<button class="btn btn-info">Delete</button></td></tr>')
+                
+                }
+            });
+            e.preventDefault();
+        }else{
+            alert('Please fill the new employee details completely');
+        }
     });
-    
   
     $('#part').click(function(){
 		let name = $('#name').val();
@@ -47,52 +51,126 @@ $(document).ready(function(){
         document.querySelector("#name").value = '';
         document.querySelector("#role").value = '';
         document.querySelector("#status").value = '';
-		if((name && role) || (name && status) ||
-		(role && status) || (name && role && status)){
-			alert("Please select only one input to search by");
-		}else{
-			if(name){
-				search="lastname";
-				searchby=name;
-			}else if(role){
-				search="role";
-				searchby=role;
-			}else{
-				search="salary-status";
-				searchby=status;
-			}
-			$.ajax({
-				type: 'GET',
-				url:   'http://localhost:3000/employees?'+search+"="+searchby,
+        if(name === '' && role === '' && status === ''){
+            alert('Please fill a search field');
+        }else{
+            if((name && role) || (name && status) ||
+            (role && status) || (name && role && status)){
+                alert("Please select only one input to search by");
+            }else{
+                if(name){
+                    search="lastname";
+                    searchby=name;
+                }else if(role){
+                    search="role";
+                    searchby=role;
+                }else{
+                    search="salary-status";
+                    searchby=status;
+                }
+                $.ajax({
+                    type: 'GET',
+                    url:   'http://localhost:3000/employees?'+search+"="+searchby,
 
-				success: function(data){
-					$('#targ').html(function(){
-						let JobsAvailable='';
-						
-						$.each(data, function(i,ignore){
-                            JobsAvailable += '<tr><td>'
-                                        +data[i]["firstname"]+'</td><td>'
-                                        +data[i]["lastname"]+'</td><td>'
-                                        +data[i]["role"]+'</td><td>'
-                                        +data[i]["salary-status"]+'</td><td>'
-                                        +'<button class="btn btn-info">Edit</button>'
-                                        +'<button class="btn btn-info">Pay</button>'
-                                        +'<button class="btn btn-info">Delete</button></td>'
-                                    '</tr>';
-    
+                    success: function(data){
+                        $('#targ').html(function(){
+                            let JobsAvailable='';
+                            
+                            $.each(data, function(i,ignore){
+                                JobsAvailable += '<tr><td>'
+                                            +data[i]["firstname"]+'</td><td>'
+                                            +data[i]["lastname"]+'</td><td>'
+                                            +data[i]["role"]+'</td><td>'
+                                            +data[i]["salary-status"]+'</td><td>'
+                                            +'<button class="btn btn-info">Edit</button>'
+                                            +'<button class="btn btn-info">Pay</button>'
+                                            +'<button class="btn btn-info">Delete</button></td>'
+                                        '</tr>';
+        
+                            })
+                            
+                        return JobsAvailable;
                         })
-                        
-					return JobsAvailable;
-					})
-					console.log('good');
-				}
-			});
-		}
-		
+                        console.log('good');
+                    }
+                });
+            }
+        }
     });
-    
-    
-   
-    
 	
 });
+
+
+function displayAll() {
+    $.ajax({
+        method: 'GET',
+        url:   'http://localhost:3000/employees',
+        success: function(data){
+            const body = $('#tblMain tbody');
+            body.empty();
+            $.each(data, function(i,element){
+                body.append('<tr><td>'+data[i]["firstname"]+'</td><td>'
+                            +data[i]["lastname"]+'</td><td>'
+                            +data[i]["role"]+'</td><td>'
+                            +data[i]["salary-status"]+'</td><td>'
+                            +'<button onclick="update(' +data[i]["id"]+ ')" class="btn btn-info">Edit</button>'
+                            +'|<button onclick="payEmployee(' +data[i]["id"]+ ')" class="btn btn-info">Pay</button>'
+                            +'|<button onclick="deleteEmployee(' +data[i]["id"]+ ')" class="btn btn-info">Delete</button></td></tr>'
+                );                             
+            });
+        }
+    })
+}
+
+function deleteEmployee(id) {
+    $.ajax({
+        url: 'http://localhost:3000/employees/'+id,
+        method: 'DELETE',
+        success: function() {
+            alert('Deleted successfuly');
+            displayAll();
+        },
+        error: function(error) {
+            alert(error);
+        }
+    });
+}
+
+function update(id) {
+    $.ajax({
+        url: 'http://localhost:3000/employees/'+id,
+        method: 'GET',
+        success: function(data){
+            $('#hidden').val(data["id"]);
+            $('#a').val(data["firstname"]);
+            $('#b').val(data["lastname"]);
+            $('#c').val(data["role"]);
+            displayAll();
+        },
+        error: function (error){
+            alert(error);
+        }
+    });
+}
+
+// function payEmployee(id) {
+//     $.ajax({
+//         url: 'http://localhost:3000/employees/'+id,
+//         method: 'PUT',
+//         success: function(data) {
+//             data[i]["salary-status"] = 'Paid';
+//             displayAll();
+//             alert('Paid');
+//         },
+//         error: function(error) {
+//             alert(error);
+//         }
+//     });
+// }
+
+function reset() {
+    $('#hidden').val() = '';
+    $('#a').val() = '';
+    $('#b').val() = '';
+    $('#c').val() = '';
+}
